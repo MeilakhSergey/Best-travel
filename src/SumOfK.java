c2850ef4911e23902b4bf5b16c05bd571330e23a
MeilakhSergey <meylakhsergey@gmail.com>
2018-11-15 18:22:04 +0300

    Begin

--- src/SumOfK.java
+++ src/SumOfK.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public class SumOfK {
+    public static void main(String[] args) {
+
+    }
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        // your code
+        return null;
+    }
 }
